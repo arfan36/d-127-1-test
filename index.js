f5505fd799b3ab047b75c5c86b061d0ea41c567e
arfan36 <arfanur36@gmail.com>
2022-11-27 14:15:14 +0600

    74-4 API naming convention and save Booking to database

--- index.js
+++ index.js
@@ -17,12 +17,30 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         const appointmentOptionCollection = client.db('doctorsPortal-D-124').collection('appointmentOptions');
+        const bookingsCollection = client.db('doctorsPortal-D-124').collection('bookings');
 
         app.get('/appointmentOptions', async (req, res) => {
             const query = {};
             const options = await appointmentOptionCollection.find(query).toArray();
             res.send(options);
         });
+
+        /* 
+        // ─── Api Naming Convention ───────────────────────────────────        
+        * app.get('/bookings')
+        * app.get('/bookings/:id')
+        * app.post('/bookings')
+        * app.patch('/bookings/:id')
+        * app.delete('/bookings/:id')
+         */
+
+        app.post('/bookings', async (req, res) => {
+            const booking = req.body;
+            console.log("🚀 ~ booking", booking);
+            const result = await bookingsCollection.insertOne(booking);
+            res.send(result);
+        });
+
     }
     finally {
 
