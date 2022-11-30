8b92c4da5962e8ae8865a811ff2c681b59225a8a
arfan36 <arfanur36@gmail.com>
2022-11-30 21:08:46 +0600

    75-9 (advanced) Prevent accessing Admin route via URL

--- index.js
+++ index.js
@@ -170,6 +170,14 @@ async function run() {
             res.send(users);
         });
 
+        // verify admin
+        app.get('/users/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email };
+            const user = await usersCollection.findOne(query);
+            res.send({ isAdmin: user?.role === 'admin' });
+        });
+
         // save user info by insertOne
         app.post('/users', async (req, res) => {
             const user = req.body;
